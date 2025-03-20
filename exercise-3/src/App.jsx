5da78a03be43eb135f7cb317bb00262afff92538
RBunna <bunnasovethroath@gmail.com>
2025-03-20 15:35:27 +0700

    Done!

--- exercise-3/src/App.jsx
+++ exercise-3/src/App.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import { useState } from "react";
 
 function App() {
   /* You will need to use many state to keep the inut values and other needs */
+  const [a , setA] = useState(0);
+  const [b , setB] = useState(0);
+  const [c , setC] = useState();
+  const [err, setErr] = useState(false);
 
   /* You will need some function to handle the key pressed and button events */
+  function onA(e) { setA(e.target.value); }
+  function onB(e) { setB(e.target.value); }
+  function add() { 
+    if (isNaN(a) || isNaN(b)) {
+      setC("A and B shall be numbers !");
+      setErr(true);
+    } else {
+    setC(parseFloat(a) + parseFloat(b)); 
+    }
+  }
 
   return (
     <main>
@@ -18,8 +33,8 @@ function App() {
       <label>A + B =</label>
 
       {/* When Compute buton is clicked, this input display the sum of the 2 numbers, or the error message in RED */}
-      <input disabled />
-      <button>Compute</button>
+      <input value={c} className={err ? "error" : ""} disabled />
+      <button onClick={add}>Compute</button>
     </main>
   );
 }
--- exercise-4/src/components/Carousel.jsx
+++ exercise-4/src/components/Carousel.jsx
@@ -1,21 +1,25 @@
 import React from "react";
+import { useState } from "react";
 import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
-
 export const Carousel = ({ images }) => {
   /* You will need to  use  state to mnage the current image */
-
+  const [img,setImg] = useState(0);
   /* You will need to hanle the click on left and right button */
-
+  function prevImg(){
+    setImg((pre)=> pre === 0 ? images.length -1 : pre - 1);
+  }
+  function nextImg(){
+    setImg((pre)=> pre === images.length-1 ? 0 : pre + 1);
+  }
   /* You will need to manage the cases when we are on the last image or first image*/
-
   return (
     <div className="carousel">
-      <BsArrowLeftCircleFill className="arrow arrow-left" />
+      <BsArrowLeftCircleFill onClick={prevImg} className="arrow arrow-left" />
 
       {/* YOu will need to display the current image, not the first one.. */}
-      <img src={images[0].src} alt={images[0].alt} className="slide" />
+      <img src={images[img].src} alt={images[img].alt} className="slide" />
 
-      <BsArrowRightCircleFill className="arrow arrow-right" />
+      <BsArrowRightCircleFill onClick={nextImg} className="arrow arrow-right" />
     </div>
   );
 };
